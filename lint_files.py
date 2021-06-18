e18fd3d4a87be5a85200ca4160c917e79b71145e
Claudio Gomes <cla.gms@gmail.com>
2021-06-18 08:54:27 +0200

    Ignore venv/ folder

--- lint_files.py
+++ lint_files.py
@@ -39,7 +39,7 @@ print("Linting files in %s" % top)
 for root, dirs, files in os.walk(top, topdown=True):
 
     # skip build, git, and cache directories
-    dirs[:] = [d for d in dirs if d not in ['build', 'fmi1_cs', 'fmi1_me', 'fmi2', 'fmi3', 'fmus']
+    dirs[:] = [d for d in dirs if d not in ['build', 'fmi1_cs', 'fmi1_me', 'fmi2', 'fmi3', 'fmus', 'venv']
                and not os.path.basename(d).startswith(('.', '_'))]
 
     for file in files:
