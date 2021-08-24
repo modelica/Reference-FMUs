aef90d2ae89dfc1e188117db5d625536fb859d3b
Torsten Sommer <torsten.sommer@3ds.com>
2021-08-24 13:25:12 +0200

    Add reference results, license, and readme to distro
    fixes #115

--- merge_platform_binaries.py
+++ merge_platform_binaries.py
@@ -1,7 +1,6 @@
 import zipfile
 from tempfile import mkdtemp
-from shutil import rmtree
-import shutil
+from shutil import rmtree, copyfile, make_archive
 import os
 
 repo_dir = os.path.dirname(__file__)
@@ -42,15 +41,30 @@ def merge(fmi_version, fmi_types):
 
             os.makedirs(os.path.dirname(merged_fmu), exist_ok=True)
 
-            shutil.make_archive(merged_fmu, 'zip', tempdir)
+            make_archive(merged_fmu, 'zip', tempdir)
 
             os.rename(merged_fmu + '.zip', merged_fmu)
 
             rmtree(tempdir, ignore_errors=True)
 
 
+# clean up
+rmtree(os.path.join(repo_dir, 'merged'), ignore_errors=True)
+
+# merge platform binaries
 merge('1.0', ['cs', 'me'])
 merge('2.0', ['cs'])
 merge('3.0', ['cs', 'se'])
 
-shutil.make_archive(os.path.join(repo_dir, 'merged_fmus'), 'zip', os.path.join(repo_dir, 'merged'))
+results_dir = os.path.join(repo_dir, 'merged', 'results')
+os.makedirs(results_dir, exist_ok=True)
+
+# copy reference results
+for model in ['BouncingBall', 'Clocks', 'Dahlquist', 'Feedthrough', 'LinearTransform', 'Resource', 'Stair', 'VanDerPol']:
+    copyfile(src=os.path.join(repo_dir, model, model + '_ref.csv'), dst=os.path.join(results_dir, model + '_ref.csv'))
+
+# copy license and readme
+for file in ['LICENSE.txt', 'README.md']:
+    copyfile(src=os.path.join(repo_dir, file), dst=os.path.join(repo_dir, 'merged', file))
+
+make_archive(os.path.join(repo_dir, 'merged_fmus'), 'zip', os.path.join(repo_dir, 'merged'))
