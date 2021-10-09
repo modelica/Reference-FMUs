a74e3445fa1887a155d1fb6a6120dded1abc10f8
Thomas Beutlich <modelica@tbeu.de>
2021-10-09 09:22:46 +0200

    Fix build workflow for set_tool_version
    Consider case where Python and Git are not on path.

--- set_tool_version.py
+++ set_tool_version.py
@@ -1,22 +1,22 @@
+import os.path
 import subprocess
 import sys
-import os
 
 
-def set_tool_version(filename):
+def set_tool_version(filename, git_executable='git'):
     """ Set the Git tag or hash in the generationTool attribute if the repo is clean """
 
     cwd = os.path.dirname(__file__)
-
-    changed_files = subprocess.check_output(['git', 'status', '--porcelain', '--untracked=no'], cwd=cwd).decode('ascii').strip()
+    
+    changed_files = subprocess.check_output([git_executable, 'status', '--porcelain', '--untracked=no'], cwd=cwd).decode('ascii').strip()
 
     if changed_files:
         return
 
-    version = subprocess.check_output(['git', 'tag', '--contains'], cwd=cwd).decode('ascii').strip()
+    version = subprocess.check_output([git_executable, 'tag', '--contains'], cwd=cwd).decode('ascii').strip()
 
     if not version:
-        version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=cwd).decode('ascii').strip()
+        version = subprocess.check_output([git_executable, 'rev-parse', '--short', 'HEAD'], cwd=cwd).decode('ascii').strip()
 
     if not version:
         return
@@ -33,6 +33,11 @@ def set_tool_version(filename):
 if __name__ == '__main__':
 
     try:
-        set_tool_version(sys.argv[1])
+        if len(sys.argv) > 2:
+            set_tool_version(sys.argv[1], sys.argv[2])
+        elif len(sys.argv) > 1:
+            set_tool_version(sys.argv[1])
+        else:
+            raise RuntimeError('Not enough arguments')
     except Exception as e:
         print(e)
