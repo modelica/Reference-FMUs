af2524ee47dab60012d77e889fc1c065ff762620
Claudio Gomes <cla.gms@gmail.com>
2022-04-05 08:33:10 +0200

    Remove whitespace.

--- examples/synchronous_control_me.c
+++ examples/synchronous_control_me.c
@@ -1,16 +1,16 @@
 /*
-The example consists of a controller and a plant, and a supervisor FMU. 
-It uses model exchange in FMI3.0. The Controller fmu declares an input periodic clock, 
-and the supervisor has an output clock that triggers when a state event occurs. 
+The example consists of a controller and a plant, and a supervisor FMU.
+It uses model exchange in FMI3.0. The Controller fmu declares an input periodic clock,
+and the supervisor has an output clock that triggers when a state event occurs.
 The output clock of the supervisor is connected to another input clock of the controller.
 
-The key part of the example is to show that both clocks can activate at the same time, 
+The key part of the example is to show that both clocks can activate at the same time,
 if the conditions are right (and the conditions are right roughly once per co-simulation).
-When that happens, the dependencies between the variables must be taken into account, 
+When that happens, the dependencies between the variables must be taken into account,
 in order to correctly propagate the values for the clocked partitions.
 
-The supervisor monitors the plant's continuous output and when a particular event happens, 
-activates a clock and changes one of the controller's inputs, 
+The supervisor monitors the plant's continuous output and when a particular event happens,
+activates a clock and changes one of the controller's inputs,
 which in turn change the controller algorithm.
 The plant is just solving a basic ODE with an input from the controller.
 
