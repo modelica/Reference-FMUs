fc1afe7e8e24e7c886941c65bbc819a4c8965420
Claudio Gomes <cla.gms@gmail.com>
2022-05-24 16:39:21 +0200

    Remove whitespace.

--- Supervisor/model.c
+++ Supervisor/model.c
@@ -44,11 +44,11 @@ Status setFloat64(ModelInstance* comp, ValueReference vr, const double *value, s
 }
 
 Status getClock(ModelInstance* comp, ValueReference vr, bool* value) {
-    
+
     // Do state event detection.
-    // It's important that getClock is invoked before getFloat64, 
+    // It's important that getClock is invoked before getFloat64,
     //  so the variable M(as) can take on the value corresponding to the event.
-    // This is ok because as is part of the clocked partition of clock s, 
+    // This is ok because as is part of the clocked partition of clock s,
     //  and therefore should not be queried when clock s is inactive.
     if (M(pz) * M(z) < 0.0 && !M(stateEvent)) {
         M(stateEvent) = true;
@@ -79,7 +79,7 @@ void eventUpdate(ModelInstance *comp) {
         M(as) = M(as) * -1.0;
         M(stateEvent) = false;
 
-        // The following has to be done in order to ensure that, 
+        // The following has to be done in order to ensure that,
         //   if the FMU remains in event mode and the getClock function gets called, it will return false.
         // This is correct as the state event should occurr only once per super dense time instant.
         M(pz) = M(z);
