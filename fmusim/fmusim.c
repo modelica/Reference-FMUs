2ddf93ceb9c6a4d780ebc91905ca790a1015c6ea
Torsten Sommer <torsten.sommer@3ds.com>
2022-11-18 15:07:21 +0100

    Set Binary and Clock start values (#307)

--- fmusim/fmusim.c
+++ fmusim/fmusim.c
@@ -124,6 +124,50 @@ void printUsage() {
     );
 }
 
+static int hexchr2bin(const char hex, char* out) {
+
+    if (out == NULL)
+        return 0;
+
+    if (hex >= '0' && hex <= '9') {
+        *out = hex - '0';
+    } else if (hex >= 'A' && hex <= 'F') {
+        *out = hex - 'A' + 10;
+    } else if (hex >= 'a' && hex <= 'f') {
+        *out = hex - 'a' + 10;
+    } else {
+        return 0;
+    }
+
+    return 1;
+}
+
+static size_t hexs2bin(const char* hex, unsigned char** out) {
+
+    size_t len;
+    char   b1;
+    char   b2;
+    size_t i;
+
+    if (hex == NULL || *hex == '\0' || out == NULL)
+        return 0;
+
+    len = strlen(hex);
+    if (len % 2 != 0)
+        return 0;
+    len /= 2;
+
+    *out = malloc(len);
+    memset(*out, 'A', len);
+    for (i = 0; i < len; i++) {
+        if (!hexchr2bin(hex[i * 2], &b1) || !hexchr2bin(hex[i * 2 + 1], &b2)) {
+            return 0;
+        }
+        (*out)[i] = (b1 << 4) | b2;
+    }
+    return len;
+}
+
 FMIStatus applyStartValues(FMIInstance* S, const FMISimulationSettings* settings) {
 
     FMIStatus status = FMIOK;
@@ -231,6 +275,22 @@ FMIStatus applyStartValues(FMIInstance* S, const FMISimulationSettings* settings
             }  if (type == FMIStringType) {
 
                 CALL(FMI3SetString(S, &vr, 1, &literal, 1));
+
+            } else if (type == FMIBinaryType) {
+
+                unsigned char* value = NULL;
+
+                const size_t size = hexs2bin(literal, &value);
+                
+                CALL(FMI3SetBinary(S, &vr, 1, &size, (fmi3Binary*)&value, 1));
+
+                free(value);
+
+            } else if (type == FMIClockType) {
+
+                const fmi3Clock value = atoi(literal) != 0;
+
+                CALL(FMI3SetClock(S, &vr, 1, &value));
 
             }
         }
