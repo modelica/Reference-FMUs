9bd89a1c5a2b00a88847064e2b7b3417e0167108
Torsten Sommer <torsten.sommer@3ds.com>
2023-06-19 15:18:50 +0200

    Set instance->time to startTime in FMI3EnterInitializationMode() (#372)
    fixes #371

--- src/FMI3.c
+++ src/FMI3.c
@@ -468,6 +468,8 @@ FMIStatus FMI3EnterInitializationMode(FMIInstance *instance,
 
     instance->state = FMI2InitializationModeState;
 
+    instance->time = startTime;
+
     CALL_ARGS(EnterInitializationMode,
         "fmi3EnterInitializationMode(toleranceDefined=%d, tolerance=%.16g, startTime=%.16g, stopTimeDefined=%d, stopTime=%.16g)",
         toleranceDefined, tolerance, startTime, stopTimeDefined, stopTime);
