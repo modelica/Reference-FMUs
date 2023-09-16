31247bd7a90cfe0c02842ae26cc063f0451d62f4
Torsten Sommer <torsten.sommer@3ds.com>
2023-09-16 16:47:36 +0200

    Check instance memory

--- src/FMI.c
+++ src/FMI.c
@@ -95,6 +95,10 @@ FMIInstance *FMICreateInstance(const char *instanceName, const char *libraryPath
 
     FMIInstance* instance = (FMIInstance*)calloc(1, sizeof(FMIInstance));
 
+    if (!instance) {
+        return NULL;
+    }
+
     instance->libraryHandle = libraryHandle;
 
     instance->logMessage = logMessage;
