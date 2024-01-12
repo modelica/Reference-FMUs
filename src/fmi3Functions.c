1e990047822874a170dc3b58dff718180aa6fcf1
Torsten Sommer <torsten.sommer@3ds.com>
2024-01-12 15:58:10 +0100

    Add parentheses around && statement

--- src/fmi3Functions.c
+++ src/fmi3Functions.c
@@ -1344,7 +1344,7 @@ fmi3Status fmi3DoStep(fmi3Instance instance,
         const fmi3Float64 nextSolverStepTime = S->time + FIXED_SOLVER_STEP;
         nextCommunicationPointReached = nextSolverStepTime > nextCommunicationPoint;
 
-        if (nextCommunicationPointReached || *eventHandlingNeeded && S->earlyReturnAllowed) {
+        if (nextCommunicationPointReached || (*eventHandlingNeeded && S->earlyReturnAllowed)) {
             break;
         }
 
