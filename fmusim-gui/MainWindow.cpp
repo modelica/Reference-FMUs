3eeac8013a9fef20de49881f4e00977a947d608a
Torsten Sommer <torsten.sommer@3ds.com>
2024-05-21 16:08:20 +0200

    Load model description

--- fmusim-gui/MainWindow.cpp
+++ fmusim-gui/MainWindow.cpp
@@ -1,11 +1,21 @@
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
 
+extern "C" {
+#include "FMIModelDescription.h"
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    FMIModelDescription* modelDescription = FMIReadModelDescription("C:\\Users\\tsr2\\Downloads\\Reference-FMUs-0.0.31\\3.0\\BouncingBall\\modelDescription.xml");
+
+    ui->pushButton->setText(modelDescription->modelName);
+
+    FMIFreeModelDescription(modelDescription);
 }
 
 MainWindow::~MainWindow()
