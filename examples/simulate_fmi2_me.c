b0f956cd533732f2c2cf178c18f1fc7bc3c00405
Guillaume Jacquenot <Gjacquenot@users.noreply.github.com>
2024-06-26 10:58:14 +0200

    Fix typos in comments (#524)

--- examples/simulate_fmi2_me.c
+++ examples/simulate_fmi2_me.c
@@ -59,7 +59,7 @@ int main(int argc, char* argv[]) {
 
     fmi2EventInfo eventInfo = { 0 };
 
-    // intial event iteration
+    // initial event iteration
     do {
 
         CALL(FMI2NewDiscreteStates(S, &eventInfo));
