68e29e890a1225420e803eeb5832d2f90e86c81a
Thomas Beutlich <modelica@tbeu.de>
2024-10-29 09:23:17 +0100

    Bump zlib to 1.3.1 (#620)

--- fmusim-gui/build/build_zlib.py
+++ fmusim-gui/build/build_zlib.py
@@ -4,8 +4,8 @@ from subprocess import check_call
 from fmpy.util import download_file
 
 
-archive = download_file('https://www.zlib.net/fossils/zlib-1.3.tar.gz',
-                        checksum='ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e')
+archive = download_file('https://www.zlib.net/fossils/zlib-1.3.1.tar.gz',
+                        checksum='9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23')
 
 root = Path(__file__).parent
 
@@ -26,7 +26,7 @@ check_call(
     cmake_args +
     ['-B', build_dir,
     '-D', f'CMAKE_INSTALL_PREFIX={ install_prefix }',
-    str(root / 'zlib-1.3')]
+    str(root / 'zlib-1.3.1')]
 )
 
 check_call([
